94b3a3b4329220b626968b8ad3bee30a34ea100d
Khomyakov Ruslan <azley@mail.ru>
2018-10-31 15:47:36 +0300

    added internet permission

---
+++ app/src/main/java/com/example/rus/tfproject/network/AuthEndpoint.java
@@ -0,0 +1,5 @@
+package com.example.rus.tfproject.network;
+
+public interface AuthEndpoint {
+
+}
---
+++ app/src/main/java/com/example/rus/tfproject/network/TFApi.java
@@ -0,0 +1,6 @@
+package com.example.rus.tfproject.network;
+
+import retrofit2.Call;
+
+public class TFApi {
+}
